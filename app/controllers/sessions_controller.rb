3240ca262f322947fbd1b9482c123392c01d2fcf
william <williamg3575467@gmail.com>
2022-11-25 22:10:25 +0000

    Create CRUD methods in appointments controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,46 @@
 class SessionsController < Devise::SessionsController
-  # before_action :authenticate_user!
+  respond_to :json
 
-    respond_to :json
-    def logged_in?
-      # binding.pry
-      if current_user
-        respond_with({ user: current_user, logged_in: true })
-      else
-        respond_with({ user: nil, logged_in: false })
-      end
-    end
-    
-    private
-    
-    def respond_with(resource, _opts = {})
-      # binding.pry
-      render json: resource
-    end
-    
-    def respond_to_on_destroy
-      log_out_success && return if current_user
-
-      log_out_failure
-    end
-
-    def log_out_success
-      render json: { message: "You are logged out." }, status: :ok
-    end
-  
-    def log_out_failure
-      render json: { message: "Hmm nothing happened."}
+  def logged_in?
+    if current_user
+      respond_with({
+        status: 200, 
+        body: {
+          user: current_user,
+          logged_in: true
+        },
+        errors: nil
+      })
+    else
+      respond_with({
+        status: 200, 
+        body: {
+          user: nil,
+          logged_in: false
+        },
+        errors: nil
+      })
     end
   end
+  
+  private
+  
+  # Buncha methods I didn't write
+  def respond_with(resource, _opts = {})
+    render json: resource
+  end
+  
+  def respond_to_on_destroy
+    log_out_success && return if current_user
+
+    log_out_failure
+  end
+
+  def log_out_success
+    render json: { message: "You are logged out." }, status: :ok
+  end
+
+  def log_out_failure
+    render json: { message: "Hmm nothing happened."}
+  end
+end
