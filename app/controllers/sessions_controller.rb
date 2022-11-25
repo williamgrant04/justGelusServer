fa811bf8d27100f8f1ca6ef40284c2cff0dc8af9
william <williamg3575467@gmail.com>
2022-11-25 21:34:36 +0000

    fix authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,34 @@
 class SessionsController < Devise::SessionsController
+  # before_action :authenticate_user!
+
     respond_to :json
-  private
+    def logged_in?
+      # binding.pry
+      if current_user
+        respond_with({ user: current_user, logged_in: true })
+      else
+        respond_with({ user: nil, logged_in: false })
+      end
+    end
+    
+    private
+    
     def respond_with(resource, _opts = {})
+      # binding.pry
       render json: resource
     end
     
     def respond_to_on_destroy
-      head :ok
+      log_out_success && return if current_user
+
+      log_out_failure
+    end
+
+    def log_out_success
+      render json: { message: "You are logged out." }, status: :ok
+    end
+  
+    def log_out_failure
+      render json: { message: "Hmm nothing happened."}
     end
   end
--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,10 @@ module Server2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'localhost:3000'
         resource(
           '*',
           headers: :any,
